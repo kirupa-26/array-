e0555af4c4fdb7ea40ff34fbddd6927cf88daf7f
KIruba <kirupanithi1@gmail.com>
2026-06-28 22:51:47 -0700

    run this program array maximum number

---
+++ src/hotel_items.java
@@ -0,0 +1,23 @@
+package Day_7_Class_Task.Array_Task;
+
+import java.util.Arrays;
+
+public class Second_Max {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+
+        int Smax = nums[0];
+        int max = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) {
+                Smax = max;
+                max = nums[i];
+            }
+        }
+
+        System.out.println("Array: " + Arrays.toString(nums));
+        System.out.println("Maximum value: " + Smax);
+    }
+}
+
